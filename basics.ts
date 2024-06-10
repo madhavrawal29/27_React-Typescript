1b61c36ca8e4ea027e8c515b88428952cb35dc0a
Madhav Rawal <112850999+madhavrawal29@users.noreply.github.com>
2024-06-10 15:51:18 +0530

    Exploring the Base Types

---
+++ basics.ts
@@ -0,0 +1,18 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Functions types, parameters
+
+
+// Primitives
+
+let age : number;
+
+age = 30;
+
+let userName : string;
+
+userName = "Madhav";
+
+let isCoach : boolean;
+
+isCoach = true;
